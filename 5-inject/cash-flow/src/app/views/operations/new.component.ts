918d28d7e7c093fc8102da5a91713ed43f49203d
Alberto Basalo <albertobasalo@hotmail.com>
2017-11-19 12:42:40 +0100

    4 clone -> 5

---
+++ 5-inject/cash-flow/src/app/views/operations/new.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { Operation } from "./operation";
+
+@Component({
+  selector: "cf-new",
+  template: `
+  <h2>{{ title | uppercase }}</h2>
+  <form class="container">
+    <label for="description">Description</label>
+    <input name="description"
+          #inputDescription
+          [value]="operation.description"
+          (change)="operation.description=inputDescription.value"
+          type="text" />
+    <label for="amount">Amount</label>
+    <input name="amount"
+          [(ngModel)]="operation.amount"
+          type="number"/>
+    <label>Kind of Operation</label>
+    <select name="kind" [(ngModel)]="operation.kind">
+      <option [value]="">Please select a kind</option>
+      <option *ngFor="let kind of kindsOfOperations"
+            [value]="kind">{{kind}}</option>
+    </select>
+    <button (click)="saveOperation()">Save</button>
+  </form>
+  <blockquote>
+    Number of Operations:{{ numberOfOperations }}
+    <em>Current: {{ operation | json }}</em>
+  </blockquote>
+  `,
+  styles: []
+})
+export class NewComponent implements OnInit {
+  @Input() public numberOfOperations = 0;
+  @Output() public save = new EventEmitter<Operation>();
+
+  public kindsOfOperations = ["Income", "Expense"];
+  public operation: Operation = new Operation();
+  public title = "New Operation";
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  public saveOperation() {
+    this.save.emit(this.operation);
+    this.operation = new Operation();
+  }
+}
