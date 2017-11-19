b44868528fd01db6f62a548d2c7f52d96800d001
Alberto Basalo <albertobasalo@hotmail.com>
2017-11-19 12:50:17 +0100

    ng g s views/operations/operations

--- 5-inject/cash-flow/src/app/views/operations/operations.module.ts
+++ 5-inject/cash-flow/src/app/views/operations/operations.module.ts
@@ -7,6 +7,7 @@ import { OperationsComponent } from "./operations.component";
 import { NewComponent } from "./new.component";
 import { ListComponent } from "./list.component";
 import { ItemComponent } from "./item.component";
+import { OperationsService } from "./operations.service";
 
 @NgModule({
   imports: [CommonModule, FormsModule, OperationsRoutingModule],
@@ -15,6 +16,7 @@ import { ItemComponent } from "./item.component";
     NewComponent,
     ListComponent,
     ItemComponent
-  ]
+  ],
+  providers: [OperationsService]
 })
 export class OperationsModule {}
---
+++ 5-inject/cash-flow/src/app/views/operations/operations.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { Operation } from "./operation";
+
+@Injectable()
+export class OperationsService {
+  private operations: Operation[] = [];
+
+  constructor() {}
+
+  public getNumberOfOperations(): number {
+    return this.operations.length;
+  }
+
+  public getOperationsList(): Operation[] {
+    return this.operations;
+  }
+
+  public saveOperation(operation: Operation) {
+    operation._id = new Date().getTime().toString();
+    this.operations.push(operation);
+  }
+
+  public deleteOperation(operation: Operation) {
+    const index = this.operations.indexOf(operation);
+    this.operations.splice(index, 1);
+  }
+}
