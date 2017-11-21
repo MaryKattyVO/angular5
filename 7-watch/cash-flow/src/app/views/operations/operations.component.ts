9dc3102f29dcd4392b19ee07c06c010d1f246275
Alberto Basalo <albertobasalo@hotmail.com>
2017-11-21 16:52:23 +0100

    6 -> 7

---
+++ 7-watch/cash-flow/src/app/views/operations/operations.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from "@angular/core";
+import { Operation } from "./operation";
+import { OperationsService } from "./operations.service";
+
+@Component({
+  selector: "cf-operations",
+  template: `
+    <cf-new 
+      [numberOfOperations]="numberOfOperations" 
+      (save)="saveOperation($event)">
+    </cf-new>
+    <cf-list 
+      [numberOfOperations]="numberOfOperations" 
+      [operations]="operations" 
+      (delete)="deleteOperation($event)" >
+    </cf-list>
+  `,
+  styles: []
+})
+export class OperationsComponent implements OnInit {
+  public numberOfOperations = 0;
+  public operations: Operation[] = [];
+  constructor(private operationsService: OperationsService) {}
+
+  ngOnInit() {
+    this.refreshData();
+  }
+
+  public saveOperation(operation: Operation) {
+    this.operationsService
+      .saveOperation$(operation)
+      .subscribe(data => this.refreshData());
+  }
+
+  public deleteOperation(operation: Operation) {
+    this.operationsService
+      .deleteOperation$(operation)
+      .subscribe(data => this.refreshData());
+  }
+
+  private refreshData() {
+    this.operationsService
+      .getOperationsList$()
+      .subscribe(data => (this.operations = data));
+    this.operationsService
+      .getNumberOfOperations$()
+      .subscribe(data => (this.numberOfOperations = data.count));
+  }
+}
