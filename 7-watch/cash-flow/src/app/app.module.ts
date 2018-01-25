63a6710a7ff6d20198b5a851046eea57619da358
Alberto Basalo <albertobasalo@hotmail.com>
2018-01-25 20:20:09 +0100

    lib component

--- 7-watch/cash-flow/src/app/app.module.ts
+++ 7-watch/cash-flow/src/app/app.module.ts
@@ -1,37 +1,19 @@
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app.routing";
 import { BrowserModule } from "@angular/platform-browser";
-import { CatchInterceptorService } from "./lib/catch-interceptor.service";
-import { ComponentsModule } from "./lib/components/components.module";
 import { HomeModule } from "./views/home/home.module";
-import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { NotFoundModule } from "./views/not-found/not-found.module";
-import { StoreService } from "./lib/store.service";
-import { TokenInterceptorService } from "./lib/token-interceptor.service";
+import { LibModule } from "./lib/lib.module";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    ComponentsModule,
     HomeModule,
-    HttpClientModule,
+    LibModule,
     NotFoundModule
-  ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptorService,
-      multi: true
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: CatchInterceptorService,
-      multi: true
-    },
-    StoreService
   ],
   bootstrap: [AppComponent]
 })
