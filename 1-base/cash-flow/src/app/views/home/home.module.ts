096ae6654a3dce646b04b53e1c2f658c56ab9c65
Alberto Basalo <albertobasalo@hotmail.com>
2018-01-29 17:13:22 +0100

    1-base

--- 1-base/cash-flow/src/app/views/home/home.module.ts
+++ 1-base/cash-flow/src/app/views/home/home.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HomeComponent } from "./home.component";
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   declarations: [HomeComponent],
   exports: [HomeComponent]
 })
-export class HomeModule { }
+export class HomeModule {}
